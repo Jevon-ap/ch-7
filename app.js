317201b3d0163bfdfd163c618b43a3c3623502d0
jevon-ap <jevonnetran@gmail.com>
2023-11-29 01:49:19 +0700

    done

--- app.js
+++ app.js
@@ -1,22 +1,68 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const authRoutes = require('./src/routes/authRoute');
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const authRoutes = require('./src/routes/authRoute')
+const Sentry = require('@sentry/node')
+const { ProfilingIntegration } = require("@sentry/profiling-node")
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/api/auth', authRoutes);
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/api/auth', authRoutes)
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0,
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+})
 
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(err.status || 500).json({
-    error: {
-      message: err.message || 'Internal Server Error',
-    },
-  });
-});
+app.use(Sentry.Handlers.requestHandler())
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler())
+
+// All your controllers should live here
+app.get("/", function rootHandler(req, res) {
+  res.end("Hello world!")
+})
+
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler())
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500
+  res.end(res.sentry + "\n")
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected :' ,socket.id)
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id)
+  })
+})
+
+const port = process.env.PORT || 8080
+
+app.get('/debug-sentry', function mainHandler(req, res) {
+  throw new Error('Testing Sentry error handling!')
+})
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
+
+
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,37 +1,100 @@
-const { PrismaClient } = require('@prisma/client');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const prisma = new PrismaClient();
+const { PrismaClient } = require('@prisma/client')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const nodemailer = require('nodemailer')
+const prisma = new PrismaClient()
 
 const register = async (req, res, next) => {
-  const { email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const { email, password } = req.body
+  const hashedPassword = await bcrypt.hash(password, 10)
   const user = await prisma.user.create({
     data: {
       email,
       password: hashedPassword,
     },
-  });
-  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-  res.status(201).json({ message: 'User registered', user, token });
-};
+  })
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' })
+  res.status(201).json({ message: 'User registered', user, token })
+}
 
 const login = async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
   const user = await prisma.user.findUnique({
     where: {
       email,
     },
-  });
+  })
   if (!user) {
-    return res.status(401).json({ message: 'Authentication failed' });
+    return res.status(401).json({ message: 'Authentication failed' })
   }
-  const match = await bcrypt.compare(password, user.password);
+  const match = await bcrypt.compare(password, user.password)
   if (!match) {
-    return res.status(401).json({ message: 'Authentication failed' });
+    return res.status(401).json({ message: 'Authentication failed' })
   }
-  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-  res.json({ message: 'Authentication successful', token });
-};
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' })
+  res.json({ message: 'Authentication successful', token })
+}
 
-module.exports = { register, login };
+const forgotPassword = async (req, res, next) => {
+  const { email } = req.body
+  const user = await prisma.user.findUnique({ where: { email } })
+  if (!user) {
+    return res.status(200).json({ message: 'If a user with that email exists, we have sent a reset link to your email.' })
+  }
+  const resetToken = (Math.random() + 1).toString(36).substring(7)
+  const hashedResetToken = await bcrypt.hash(resetToken, 10)
+  const resetTokenExpiration = new Date(Date.now() + 3600000)
+  await prisma.user.update({
+    where: { email },
+    data: {
+      resetToken: hashedResetToken,
+      resetTokenExpiration,
+    },
+  })
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  })
+  const mailOptions = {
+    from: process.env.EMAIL_USERNAME,
+    to: process.env.EMAIL_USERNAME,
+    subject: 'Password Reset',
+    text: `Your password reset token is: ${resetToken}`,
+  }
+  await transporter.sendMail(mailOptions)
+  res.status(200).json({ message: 'we have sent a reset link to your email.' })
+}
+
+const resetPassword = async (req, res, next) => {
+  const { email, token, newPassword } = req.body
+  const user = await prisma.user.findUnique({
+    where: { email },
+  })
+  if (!user || !user.resetTokenExpiration || user.resetTokenExpiration < new Date()) {
+    return res.status(400).json({ message: 'Invalid or expired password reset token.' })
+  }
+  const tokenMatches = await bcrypt.compare(token, user.resetToken)
+  if (!tokenMatches) {
+    return res.status(400).json({ message: 'Invalid or expired password reset token.' })
+  }
+  const hashedNewPassword = await bcrypt.hash(newPassword, 10)
+  await prisma.user.update({
+    where: { email },
+    data: {
+      password: hashedNewPassword,
+      resetToken: null,
+      resetTokenExpiration: null,
+    },
+  })
+  res.status(200).json({ message: 'Your password has been successfully reset.' })
+}
+
+module.exports = { 
+  register,
+  login,
+  forgotPassword,
+  resetPassword
+}
